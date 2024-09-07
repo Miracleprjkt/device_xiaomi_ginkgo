fa2fae739698ff6a568318095f8a535a9258b898
Miraclev1 <Miraclev1@proton.me>
2024-09-07 11:39:36 +0700

    ginkgo:fix path for releasetools
    *add OTA installed on releasetool
    *fix data input zip
    *fix def ota_installed
    
    Signed-off-by: Miraclev1 <Miraclev1@proton.me>

--- releasetools.py
+++ releasetools.py
@@ -1,7 +1,5 @@
-#
 # Copyright (C) 2009 The Android Open Source Project
-# Copyright (C) 2019 The Mokee Open Source Project
-# Copyright (C) 2020-2021 The LineageOS Open Source Project
+# Copyright (c) 2011, The Linux Foundation. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,31 +12,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 import common
 import re
 
+def FullOTA_Assertions(info):
+  input_zip = info.input_zip
+  return
+
+def IncrementalOTA_Assertions(info):
+  input_zip = info.target_zip
+  return
+
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  input_zip = info.input_zip
+  OTA_InstallEnd(info, input_zip)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  input_zip = info.target_zip
+  OTA_InstallEnd(info, input_zip)
   return
 
-def AddImage(info, basename, dest):
+def AddImage(info, input_zip, basename, dest):
   name = basename
-  path = "IMAGES/" + name
-  if path not in info.input_zip.namelist():
-    return
-
-  data = info.input_zip.read(path)
+  data = input_zip.read("IMAGES/" + basename)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
-def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+def OTA_InstallEnd(info, input_zip):
+  AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   return
