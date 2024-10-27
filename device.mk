d852cc6228d60217466fa4940e6b3df5d0145062
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 16:57:40 -0400

    ginkgo: Ensure we build the vendor version of libcrypto_shim
    Reference: Ia42de59ff157cddc930a111f8ce5e36e645055ed
    Change-Id: I9c9340b37a1ad0d6c22d3947541fe1196a5efeec
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
     libcrypto-v33 \
     android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor \
-    libcrypto_shim
+    libcrypto_shim.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
