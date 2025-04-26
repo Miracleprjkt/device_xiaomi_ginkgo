068711291efc635d151b541dac0727c03deaef6f
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-04-26 13:14:55 +0000

    Revert "ginkgo: Update telephony from LA.QSSI.12.0.r1-06500"
    This reverts commit 47737883fda2718c108d4a2e3302dc235732cab1.
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -420,8 +420,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 # Telephony
 PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
