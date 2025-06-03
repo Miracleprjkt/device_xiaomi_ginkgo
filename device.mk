e0910e928caf33929c13e61ed23c6b71ae6d4ecb
Miracleprjkt <Miraclev1@proton.me>
2025-06-03 18:14:19 +0700

    ginkgo: change config boot-image to boot
    [31merror:[0m packages/modules/RuntimeI18n/apex/Android.bp:65:1: module i18n-bootclasspath-fragment variant android_common: source path frameworks/base/config/boot-image-profile.txt does not exist
    [31merror:[0m art/build/boot/Android.bp:41:1: module art-bootclasspath-fragment variant android_common: source path frameworks/base/config/boot-image-profile.txt does not exist
    
    Signed-off-by: Miracleprjkt <Miraclev1@proton.me>

--- device.mk
+++ device.mk
@@ -412,7 +412,7 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 # Use a profile based boot image for this device
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Telephony
 PRODUCT_PACKAGES += \
