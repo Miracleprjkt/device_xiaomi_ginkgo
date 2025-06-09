9b6412f2c25d7ad0a13bf1d0123c4b4aa6f6cefe
ZorEl212 <yabsirad212@gmail.com>
2025-06-09 02:29:28 -0400

    ginkgo: Set vendor init lib via soong config
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>
    Signed-off-by: Miracleprjkt <Miraclev1@proton.me>

--- device.mk
+++ device.mk
@@ -255,6 +255,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Libinit
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_ginkgo)
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.ginkgo
